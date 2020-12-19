e0a50e67fbf5d5a876a40c20413857d59ae02243
yandmtech <65448292+yandmtech@users.noreply.github.com>
2020-12-19 13:17:59 +0100

    1

---
+++ coso/Assets/scripts1/objectdetect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectdetect : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 60, Color.red);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) 
+        {
+            Get          
+        }
+    }
+}
